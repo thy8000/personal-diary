6906cf1ae6ae933392adc08c7441e3c161d35db3
thy8000 <thunaymoreiradesoares@gmail.com>
2025-01-21 17:55:18 -0300

    🚧 Login Page & Header

---
+++ personal-diary/src/app/login/page.tsx
@@ -0,0 +1,13 @@
+'use client'
+
+export default function Login() {
+   return `
+      <div>
+         Login
+      </div>
+
+      <div>
+         AAAAAAAAAAA
+      </div>
+      `
+}
--- personal-diary/src/app/page.tsx
+++ personal-diary/src/app/page.tsx
@@ -1,5 +1,18 @@
+'use client'
+
+import {useEffect} from 'react'
+import {useRouter} from 'next/navigation'
+
+function redirectLogin(router) {
+   router.push('/login')
+}
+
 export default function Home() {
-  return (
-      <div>Front page - Hello World</div>
-  );
+   const router = useRouter()
+
+   useEffect(() => {
+      redirectLogin(router)
+   })
+
+   return null
 }
